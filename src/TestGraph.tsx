3a2f5045e243ae566c102a41001babc44a2f5079
Valde <randomvald0069@gmail.com>
2020-03-07 23:27:24 +0100

    Composable

--- src/TestGraph.tsx
+++ src/TestGraph.tsx
@@ -22,10 +22,73 @@ export default class TestGraph extends React.Component {
     }
 
     render() {
+        const lines: Line[] = [
+            {
+                source: {
+                    x: 1,
+                    y: 1
+                },
+                sink: {
+                    x: 500,
+                    y: 500
+                }
+            }
+        ];
+
         return (
             <svg className="container" ref={(ref: SVGSVGElement) => this.ref = ref}
                  width={this.width} height={this.height}>
+                <LinesC lines={lines} />
             </svg>
         );
+    }
+}
+
+interface Point {
+    x: number,
+    y: number
+}
+
+interface Line {
+    source: Point,
+    sink: Point
+}
+
+export class LinesC extends React.Component<{lines: Line[]}, {}> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        const childLines: React.ReactNode[] = this.props.lines.map((line: Line, index: number) => {
+            return <LineC line={line} key={index}/>;
+        });
+
+        return (
+            <g className={"lines"}>
+                {childLines}
+            </g>
+        );
+    }
+}
+
+class LineC extends React.Component<{line: Line}, {}> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    ref: SVGSVGElement = SVGSVGElement.prototype;
+
+    componentDidMount() {
+        const asLine: string | null = d3.line()([[this.props.line.source.x, this.props.line.source.y], [this.props.line.sink.x, this.props.line.sink.y]]) as string;
+
+        d3.select(this.ref)
+            .append("path")
+            .attr("stroke", "black")
+            .attr("d", asLine)
+    }
+
+    render() {
+        return <svg width={200} height={200} className={"link"} ref={(ref: SVGSVGElement) => this.ref = ref}/>;
     }
 }
